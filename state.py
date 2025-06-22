29410f072ae2080e914117b2a5707bc441001693
leske42 <mria.marika.huszr@gmail.com>
2025-06-22 17:19:35 +0200

    feat: implement transition logic

--- state.py
+++ state.py
@@ -9,7 +9,18 @@ class Machine:
         self.pos = 0 # TODO: make this configurable
     
     def run(self):
-        print(self.map)
+
+        while True: #TODO how not to run out of infinite tape bounds?
+            if self.pos < 0 or self.pos >= len(self.tape): #this HALTS if we arrive at left or right blank which is not correct behavior
+                break
+            if self.state not in self.map or self.tape[self.pos] not in self.map[self.state]:
+                break
+            to_state, write_symbol, mov = self.map[self.state][self.tape[self.pos]]
+            self.state = to_state
+            self.tape[self.pos] = write_symbol
+            self.pos += -1 if mov == "LEFT" else (1 if mov == "RIGHT" else 0)
+
+        print(self.tape)
 
 
 if __name__ == "__main__":
@@ -18,4 +29,3 @@ if __name__ == "__main__":
         sys.exit(1)
     config = parse_config(sys.argv[1])
     Machine(config["transitions"], config["init"], config["tape"]).run()
-
